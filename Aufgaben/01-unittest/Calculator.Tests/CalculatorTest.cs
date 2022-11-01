932b871b580b7ee960283fd348752c008ea38c8b
NewaraTaiyo <julia@stadler.li>
2022-11-01 22:13:18 +0100

    Completed up to exercise 3

--- Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
+++ Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
@@ -6,12 +6,11 @@ namespace Calculator.Tests
     public class CalculatorTest
     {
         [Fact]
-        public void TestAddThreeToTwoPIsFive()
-        {
+        public void TestAddThreeToTwoPIsFive() {
             // Arrange
-            int a = 2;
+            int a = 7;
             int b = 3;
-            int expected = 5;
+            int expected = 10;
             Calculator calculator = new Calculator();
 
             // Act
@@ -22,12 +21,11 @@ namespace Calculator.Tests
         }
 
         [Fact]
-        public void TestSubtractFourFromSixIsTwo()
-        {
+        public void TestSubtractFourFromSixIsTwo() {
             // Given
             int a = 6;
-            int b = 4;
-            int expected = 2;
+            int b = 5;
+            int expected = 1;
             Calculator calculator = new Calculator();
 
             // When
@@ -35,6 +33,39 @@ namespace Calculator.Tests
 
             // Then
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void multiplyFourByEightIsThirtyTwo() {
+            int factorA = 4;
+            int factorB = 8;
+            Calculator calculator = new Calculator();
+
+            int result = calculator.multiply(factorA, factorB);
+
+            Assert.Equal(32, result);
+        }
+
+        [Fact]
+        public void divideTwentyByFiveIsFour() {
+            int dividend = 20;
+            int divisor = 5;
+            Calculator calculator = new Calculator();
+
+            int result = calculator.divide(dividend, divisor);
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void divideByZeroThrowsExeption() {
+            int dividend = 20;
+            int divisor = 0;
+            Calculator calculator = new Calculator();
+
+            Assert.Throws<ArgumentException>(() => 
+                calculator.divide(dividend, divisor)
+            );
         }
     }
 }
--- Aufgaben/01-unittest/Calculator/Calculator.cs
+++ Aufgaben/01-unittest/Calculator/Calculator.cs
@@ -4,14 +4,26 @@ namespace Calculator
 {
     public class Calculator
     {
-        public int Add(int summandA, int summandB)
-        {
+        public int Add(int summandA, int summandB) {
             return summandA + summandB;
         }
 
-        public int Subtract(int minuend, int subtrahend)
-        {
+        public int Subtract(int minuend, int subtrahend) {
             return minuend - subtrahend;
+        }
+
+        public int multiply(int factorA, int factorB) {
+            return factorA * factorB;
+        }
+
+        public int divide(int dividend, int divisor) {
+            int result = 0;
+            try{
+                result = dividend / divisor;
+            } catch(DivideByZeroException e) {
+                throw new ArgumentException();
+            }
+            return result;
         }
     }
 }
