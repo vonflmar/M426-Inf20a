559bbddedaef8dbb17ecccbe81471bbae5bda1df
Nicolas von Rotz <nicolas_vonrotz@sluz.ch>
2022-11-01 19:07:26 +0100

    added Median with tests and tests for Mean

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,13 +9,67 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 1, 5};
+            double expected = 3;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5};
+            double expected = 3;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfNoNumbers()
+        {
+            List<int> numbers = new List<int> {};
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Mean(numbers));
+        }
+
+        [Fact] 
+        public void TestMedianForOddNumberOfElements()
+        {
+            int[] numbers = { 1, 5, 9, 6, 3 };
+            int expected = 5;
+            Average average = new Average();
+
+            double actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact] 
+        public void TestMedianForEvenNumberOfElements()
+        {
+            int[] numbers = { 1, 5, 9, 7, 6, 3 };
+            double expected = 5.5;
+            Average average = new Average();
+
+            double actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact] 
+        public void TestMedianForNoElements()
+        {
+            int[] numbers = {};
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Median(numbers));
         }
     }
 }
--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
@@ -10,6 +11,8 @@ namespace Statistics
             int sum = 0;
             double mean = 0.0;
 
+            if (numbers.Count == 0) throw new ArgumentException();
+
             foreach (int number in numbers)
             {
                 sum += number;
@@ -17,6 +20,23 @@ namespace Statistics
 
             mean = (double)sum / numbers.Count;
             return mean;
+        }
+
+        public double Median(int[] numbers)
+        {
+            if (numbers.Length == 0) throw new ArgumentException();
+
+            Array.Sort(numbers);
+
+            if (numbers.Length % 2 != 0)
+            {
+                int mid = (numbers.Length - 1) / 2;
+                return numbers[mid];
+            }
+
+            int value1 = numbers.Length / 2;
+            int value2 = numbers.Length / 2 - 1;
+            return this.Mean(new List<int> { numbers[value1], numbers[value2] }); 
         }
     }
 }
