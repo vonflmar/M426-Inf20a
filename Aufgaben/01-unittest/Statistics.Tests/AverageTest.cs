c0fccbea6911ab79ca7200947bb7d3fee49012b6
BurokkuDonut <faesschen06hartweizen@icloud.com>
2022-11-02 08:17:22 +0100

    testfall für 5 nummern geschrieben

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,8 +9,19 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 2, 4 };
+            double expected = 3;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 2, 4, 4, 5, 6 };
+            double expected = 4.2;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
