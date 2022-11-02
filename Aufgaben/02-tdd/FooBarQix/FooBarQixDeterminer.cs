f7c6cf937f1bd8885f457cb37723d17a1eea317e
BurokkuDonut <faesschen06hartweizen@icloud.com>
2022-11-02 13:44:56 +0100

    Test für FooBar implementiert

--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,26 @@
 ﻿namespace FooBarQix
 {
-    public class FooBarQixDeterminer
+    public string Determine(int input)
     {
 
+        string res = "";
+
+        if (input % 3 == 0)
+        {
+            res += "Foo";
+        }
+        if (input % 5 == 0)
+        {
+            res += "Bar";
+        }
+        if (input % 7 == 0)
+        {
+            res += "Qix";
+        }
+        if (res == "")
+        {
+            res = input.ToString();
+        }
+        return res;
     }
 }
