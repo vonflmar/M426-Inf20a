8560640c00d9e72cd01bdc0f99e1369e2dc4fa02
BurokkuDonut <faesschen06hartweizen@icloud.com>
2022-11-02 09:14:36 +0100

    Methode Median implementiert

--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -10,13 +10,29 @@ namespace Statistics
             int sum = 0;
             double mean = 0.0;
 
+            // if(numbers.count == 0){
+            //     throw new ArgumentException;
+            // }
+
             foreach (int number in numbers)
             {
                 sum += number;
             }
 
+            
+
             mean = (double)sum / numbers.Count;
             return mean;
+        }
+        public double Median(List<int> numbers){
+            int median = 0;
+            int halfAmount = numbers.Count / 2;
+
+            for(int i = 0; i < halfAmount; i++){
+                median = numbers[i];
+            }
+
+            return median;
         }
     }
 }
