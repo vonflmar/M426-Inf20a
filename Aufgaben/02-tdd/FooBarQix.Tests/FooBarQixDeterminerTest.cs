027a630cf1fac702b752148378269938b435f6b0
NewaraTaiyo <julia@stadler.li>
2022-11-08 22:57:07 +0100

    finish exercise

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace FooBarQix.Tests
@@ -5,8 +6,63 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
-        {
+        public void determine_numberTwo_returnsNumberTwo() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "2";
+
+            string result = fooBarQix.determine(2);
+
+            Assert.Equals(expected, result);
+        }
+
+        [Fact]
+        public void determine_numberThree_returnsFoo() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "Foo";
+
+            string result = fooBarQix.determine(3);
+
+            Assert.Equals(expected, result);
+        }
+
+        [Fact]
+        public void determine_numberFive_returnsBar() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "Bar";
+
+            string result = fooBarQix.determine(5);
+
+            Assert.Equals(expected, result);
+        }
+
+        [Fact]
+        public void determine_numberSix_returnsFoo() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "Foo";
+
+            string result = fooBarQix.determine(6);
+
+            Assert.Equals(expected, result);
+        }
+
+        [Fact]
+        public void determine_numberSeven_returnsQix() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "Qix";
+
+            string result = fooBarQix.determine(7);
+
+            Assert.Equals(expected, result);
+        }
+
+        [Fact]
+        public void determine_number105_returnsFooBarQix() {
+            FooBarQixDeterminer fooBarQix = new FooBarQixDeterminer();
+            string expected = "FooBarQix";
+
+            string result = fooBarQix.determine(105);
+
+            Assert.Equals(expected, result);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,19 @@
 ﻿namespace FooBarQix
 {
-    public class FooBarQixDeterminer
-    {
+    public class FooBarQixDeterminer {
+        public string determine(int number) {
+            string result = "";
+            if((number % 3) == 0) {
+                result += "Foo";
+            } else if((number % 5) == 0) {
+                result += "Bar";
+            } else if((number % 7) == 0) {
+                result += "Qix";
+            } else {
+                result += number;
+            } 
+            return result;
+        }
 
     }
 }
